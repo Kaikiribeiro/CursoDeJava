af47d88175faf30f9d057b9278a9ab657d817349
Kaiki Ribeiro <kaikiribeiro943@gmail.com>
2026-01-09 18:16:09 -0300

    feat: Adicionando Arquivos Anteriormente ja feito

--- src/main/java/fundamentos/PrimeiroPrograma.java
+++ src/main/java/fundamentos/PrimeiroPrograma.java
@@ -3,6 +3,6 @@ package fundamentos;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         //Uma sentença de código termina com;
-        System.out.println("Hello Word!");
+        System.out.println("Hello World!");
     }
 }
