c9f3c842b0f270c146b4eb5431807ccb75afec50
Kaiki Ribeiro <kaikiribeiro943@gmail.com>
2026-01-09 17:28:34 -0300

    fix: Corrigindo pastas

--- src/main/java/fundamentos/Main.java
+++ src/main/java/fundamentos/Main.java
@@ -1,6 +1,8 @@
+package fundamentos;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Word!I");
+        System.out.println("Hello World!");
         System.out.println("alo");
     }
 }
